af50b2eb9a78fddb7714718ffe3b95f9978ed212
Yuriy <yurivano@yandex.ru>
2023-11-07 11:12:57 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://dddb7cc0-c3c7-43be-abdf-ca7fcd12cee4.serverhub.praktikum-services.ru"
+PRODUCTS_KITS_PATH ="/api/v1/products/kits/"
+USERS_TABLE_PATH ="/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
